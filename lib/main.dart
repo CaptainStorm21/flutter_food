c16a92cdaabd7f9f64f8a56f94429cea9b79a180
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-06-22 20:20:32 -0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_food/screens/home_screen.dart';
+import 'package:grocery_app/provider/dark_theme_provider.dart';
+import 'package:grocery_app/screens/home_screen.dart';
 
-import 'conssts/theme_dark.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  //video 12 1:30
-  //video 12 2:30 removed     DeviceOrientation.portraitDown
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp
-  ]);
-  runApp(new MyApp());
+import 'consts/theme_dark.dart';
+
+void main() {
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool _isDark = true;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: Styles.themeData(true, context),
-      home:  HomeScreen()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: HomeScreen());
+      }),
     );
   }
 }
--- lib/services/dark_theme_prefs.dart
+++ lib/services/dark_theme_prefs.dart
@@ -1,14 +1,15 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DarkThemePrefs {
-  static const THEME_STATUS = "THEME_STATUS";
+  static const THEME_STATUS = "THEMESTATUS";
+
   setDarkTheme(bool value) async {
-    SharedPreferences prefs= await SharedPreferences.getInstance();
-    prefs.setBool(THEME_STATUS,  value);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(THEME_STATUS, value);
   }
 
-  Future <bool> getTheme()async{
-    SharedPreferences prefs= await SharedPreferences.getInstance();
-return prefs.getBool(THEME_STATUS) ?? false;
+  Future<bool> getTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(THEME_STATUS) ?? false;
   }
 }
